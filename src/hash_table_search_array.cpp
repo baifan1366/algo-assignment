53dca39fc72e5cbd98142b60b6772a9a6fdd35c8
Tan Jing Heng <104840897+TJH867628@users.noreply.github.com>
2026-06-18 21:14:38 +0800

    add 10m dataset

--- src/hash_table_search_array.cpp
+++ src/hash_table_search_array.cpp
@@ -343,8 +343,8 @@ int main()
         // std::string input_path = "../dataset_10000.csv";
         // std::string input_path = "../dataset_50000.csv";
         // std::string input_path = "../dataset_100000.csv";
-        // std::string input_path = "../dataset_250000.csv";
-        std::string input_path = "../dataset_500000.csv";
+        std::string input_path = "../dataset_250000.csv";
+        // std::string input_path = "../dataset_500000.csv";
         // std::string input_path = "../dataset_1000000.csv";
         // std::string input_path = "../dataset_2500000.csv";
         // std::string input_path = "../dataset_5000000.csv";
